5994c9a33a9d088ddd4437d63527ca0822cdeecf
Lucas2981 <lucas298113@gmail.com>
2024-02-11 18:59:14 -0300

    user

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+    python material_crud/manage.py createsuperuser --no-input
+fi
