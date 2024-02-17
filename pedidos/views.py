cf17218b90c578e5bff1df1342e880dbd12647f7
Lucas2981 <lucas298113@gmail.com>
2024-02-17 20:01:03 -0300

    iconos, filtro y memoria

--- pedidos/views.py
+++ pedidos/views.py
@@ -8,6 +8,8 @@ from django.db.models import Q
 from django.db.models import Sum
 from django.http import HttpResponse
 import pandas as pd
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
 
 
 def index(request):
@@ -58,7 +60,8 @@ def pedidosDir(request):
             Q(obra__name__icontains=queryset) |
             Q(user__username__icontains=queryset)|
             Q(orden_compra__icontains=queryset)|
-            Q(id__icontains=queryset),
+            Q(id__icontains=queryset)|
+            Q(memoria__icontains=queryset),
             ).distinct().order_by('validated', 'id')
     return render(request, 'pedidoDir.html', {'pedidos': pedidos, })
 @login_required
@@ -72,8 +75,9 @@ def pedidos_materialespedido(request, pedido_id):
     pedidos_materialespedido = list(pedidos_agrupados)
     # Convertir el resultado a una lista
     pedidos_materialespedido = list(pedidos_agrupados)
-    obra_name = pedidos_materialespedido[0]['materialespedido__pedido__obra__name'].capitalize()
+    obra_name = pedidos_materialespedido[0]['materialespedido__pedido__obra__name'].title()
     cod_pedido = 'P'+str(pedidos_materialespedido[0]['materialespedido__pedido'])+pedidos_materialespedido[0]['materialespedido__pedido__user__username'][:4].upper()
+    ubicacion = pedidos_materiales.values_list('materialespedido__pedido__obra__localidad')[0][0].title()+', Dpto. '+pedidos_materiales.values_list('materialespedido__pedido__obra__dpto')[0][0].title()
     if request.method == 'GET':
         pedido = get_object_or_404(Pedido, pk=pedido_id)
         form = PedidoForm(instance=pedido)
@@ -82,6 +86,7 @@ def pedidos_materialespedido(request, pedido_id):
             'form': form,
             'obra_name': obra_name,
             'cod_pedido': cod_pedido,
+            'ubicacion': ubicacion
             })
     else:
         try:
@@ -97,6 +102,7 @@ def pedidos_materialespedido(request, pedido_id):
             'form': form,
             'obra_name': obra_name,
             'cod_pedido': cod_pedido,
+            'ubicacion': ubicacion,
             'error': 'Cod de pedido ya existe.'
             })
 
@@ -109,10 +115,11 @@ def pedidos_materialespedidoDir(request, pedido_id):
     pedidos_agrupados = pedidos_materiales.values('materialespedido__pedido','materialespedido__pedido__user__username' ,'materialespedido__material__name','materialespedido__material__unidad','materialespedido__pedido__obra__name', 'materialespedido__material__rubro__name', 'materialespedido__sector__name').annotate(cantidad=Sum('materialespedido__cantidad')).order_by('materialespedido__sector__name','materialespedido__material__name')
     # Convertir el resultado a una lista
     pedidos_materialespedido = list(pedidos_agrupados)
-    obra_name = pedidos_materialespedido[0]['materialespedido__pedido__obra__name'].capitalize()
+    obra_name = pedidos_materialespedido[0]['materialespedido__pedido__obra__name'].title()
     cod_pedido = 'P'+str(pedidos_materialespedido[0]['materialespedido__pedido'])+pedidos_materialespedido[0]['materialespedido__pedido__user__username'][:4].upper()
     a_proveedor = pedidos_materiales.values_list('materialespedido__pedido__a_proveedor')[0][0]
-    ubicacion = pedidos_materiales.values_list('materialespedido__pedido__obra__localidad')[0][0]+', Dpto. '+pedidos_materiales.values_list('materialespedido__pedido__obra__dpto')[0][0]
+    ubicacion = pedidos_materiales.values_list('materialespedido__pedido__obra__localidad')[0][0].title()+', Dpto. '+pedidos_materiales.values_list('materialespedido__pedido__obra__dpto')[0][0].title()
+    memoria = pedidos_materiales.values_list('materialespedido__pedido__memoria')[0][0]
     if request.method == 'GET':
         pedido = get_object_or_404(Pedido, pk=pedido_id)
         form = PedidoFormDir(instance=pedido)
@@ -122,7 +129,8 @@ def pedidos_materialespedidoDir(request, pedido_id):
             'obra_name': obra_name,
             'cod_pedido': cod_pedido,
             'a_proveedor': a_proveedor,
-            'ubicacion': ubicacion
+            'ubicacion': ubicacion,
+            'memoria': memoria
             })
     else:
         try:
@@ -140,10 +148,11 @@ def pedidos_materialespedidoDir(request, pedido_id):
             'cod_pedido': cod_pedido,
             'a_proveedor': a_proveedor,
             'ubicacion': ubicacion,
+            'memoria': memoria,
             'error': 'Error al validar pedido.'
             })
 @login_required
-def generar_archivo_xls(request,pedido_id): #aqui falta agregar columna de rubro
+def generar_archivo_xls(request,pedido_id): 
     try:
         # Obtener todos los pedidos de materiales
         pedidos_materiales = Pedido.objects.filter(pk=pedido_id)
@@ -153,13 +162,14 @@ def generar_archivo_xls(request,pedido_id): #aqui falta agregar columna de rubro
         pedidos_materialespedido = list(pedidos_agrupados)
         pedidos_df = pd.DataFrame(pedidos_materialespedido)
         cod_pedido = 'P'+str(pedidos_materialespedido[0]['materialespedido__pedido'])+pedidos_materialespedido[0]['materialespedido__pedido__user__username'][:4].upper()
-        df = pedidos_df.rename(columns={'materialespedido__pedido__obra__name':'Obra','materialespedido__pedido':'Pedido N°','materialespedido__material__name':'Material','materialespedido__material__unidad':'Unidad','cantidad':'Cant. Solicitada','materialespedido__material__rubro__name':'Rubro'})
-        # df = df['Pedido N°'] == pk
-        df = df[['Rubro','Material','Unidad','Cant. Solicitada']]
-        df = df.sort_values(['Rubro','Material'])
+        df = pedidos_df.rename(columns={'materialespedido__pedido__obra__name':'Obra','materialespedido__pedido':'Pedido N°','materialespedido__material__name':'Descripción','materialespedido__material__unidad':'Unidad de medida','cantidad':'Cantidad','materialespedido__material__rubro__name':'Observaciones'})
+
+        df = df[['Descripción','Unidad de medida','Cantidad','Observaciones']]
+        df = df.sort_values(['Observaciones','Descripción'])
         
         response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-        response['Content-Disposition'] = f'attachment; filename="pedido_{pedidos_df["materialespedido__pedido__obra__name"][0]}.xlsx"'
+        response['Content-Disposition'] = f'attachment; filename="Pedido {pedidos_df["materialespedido__pedido__obra__name"][0].title()}.xlsx"'
+        
         writer = pd.ExcelWriter(response)
         df.to_excel(writer, sheet_name=cod_pedido, index=False)
         for column in df:
