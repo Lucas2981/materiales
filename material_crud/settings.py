ae74fc3b6aa77ffc40085df2379e29da22fb2113
Lucas2981 <lucas298113@gmail.com>
2024-03-17 22:20:13 -0300

    mas

--- material_crud/settings.py
+++ material_crud/settings.py
@@ -99,8 +99,8 @@ WSGI_APPLICATION = "material_crud.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600)}
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3","NAME": BASE_DIR / "db.sqlite3",}}
+DATABASES = {'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600)}
+# DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3","NAME": BASE_DIR / "db.sqlite3",}}
 
 
 # Password validation
